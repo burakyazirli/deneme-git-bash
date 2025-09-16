3717262b88c2e23dae57b1a1292806f3b1905f84
burakyazÅirli <brkyzrl@outlook.com>
2025-09-17 01:29:05 +0300

    first commit test

---
+++ src/main/java/com/test1/spr_intellij_test1/SprIntellijTest1Application.java
@@ -0,0 +1,13 @@
+package com.test1.spr_intellij_test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprIntellijTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprIntellijTest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/test1/spr_intellij_test1/SprIntellijTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test1.spr_intellij_test1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprIntellijTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
